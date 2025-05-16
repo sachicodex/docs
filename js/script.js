64e4654694d0338513ee38039b29ffc665ec05c7
sachicodex <hello.sachinthalakshan@gmail.com>
2025-05-16 11:43:14 +0530

    Add initial website structure and styles
    - Created index.html with meta tags, structured data, and preloader.
    - Added desktop.css for styling the main layout and typography.
    - Included mobile.css and style.css as placeholders for future styles.
    - Implemented script.js for navigation and header animations.
    - Integrated various Google Fonts and icons for enhanced UI.
    - Established footer section with social media links and developer tools.

---
+++ js/script.js
@@ -0,0 +1,47 @@
+function openNav() {
+  navBar = document.getElementById('nav')
+
+  navBar.style.display = 'block';
+}
+
+function closeNav() {
+  navBar = document.getElementById('nav')
+
+  navBar.style.display = 'none';
+}
+
+function showHeader() {
+  const header = document.getElementById('content');
+  header.classList.add('active');
+}
+
+window.onload = () => {
+  setTimeout(showHeader, 5700);
+};
+
+// Header Animation
+
+document.addEventListener('DOMContentLoaded', function () {
+  const headerLis = document.querySelectorAll('header ul li');
+  const defaultActiveLi = document.querySelector('header ul li:nth-child(2)');
+  const header = document.querySelector('header');
+
+  let activeLi = defaultActiveLi;
+  activeLi.classList.add('active-item');
+
+  headerLis.forEach(li => {
+    li.addEventListener('mouseover', function () {
+      if (activeLi !== li) {
+        activeLi.classList.remove('active-item');
+      }
+      activeLi = li;
+      li.classList.add('active-item');
+    });
+
+    li.addEventListener('mouseout', function () {
+      activeLi.classList.remove('active-item');
+      defaultActiveLi.classList.add('active-item');
+      activeLi = defaultActiveLi;
+    });
+  });
+});
